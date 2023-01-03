9f00f557b4577e3b01dada784d8aa717072df0c1
bojanlukic <bokalukic1990@gmail.com>
2023-01-03 13:00:46 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  // const [task, setTask] = useState([])
+  const [task, setTask] = useState(0)
+  const [name, setName] = useState('')
 
-
+  const onClick = () => {
+    setTask(name)
+  }
 
 
   return (
     <div className="App">
       <h1 className="title"> <span>Trello</span> Like Board</h1>
-      <button className="taskButton">Add Task</button>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <button className="taskButton" onClick={onClick}>Add Task</button>
       <div className="container">
         <div >To do</div>
         <div>In Progress</div>
         <div>Done</div>
       </div>
       <div className="container2">
-        <div ></div>
+        <div>{task}</div>
         <div></div>
         <div></div>
       </div>
